fc68b8134f943070f82398dec5d7e72bac0f99ca
Hilary Lamb <hilaryklamb@gmail.com>
2020-03-27 20:08:29 -0400

    input id and encryption added

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hi there');
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  document.querySelector('#link-input').value = encrypted;
+});
